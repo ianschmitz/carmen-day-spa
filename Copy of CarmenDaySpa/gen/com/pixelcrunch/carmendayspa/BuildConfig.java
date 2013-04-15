4d97002960e5f3a52297f4e2ed68c14d5bb85ac1
SeansAir <sean1991stewart@gmail.com>
2013-04-15 08:41:26 -0700

    Start of App Development

---
+++ Copy of CarmenDaySpa/gen/com/pixelcrunch/carmendayspa/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pixelcrunch.carmendayspa;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Copy of CarmenDaySpa/src/com/pixelcrunch/carmendayspa/Home.java
@@ -0,0 +1,22 @@
+package com.pixelcrunch.carmendayspa;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard_layout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
