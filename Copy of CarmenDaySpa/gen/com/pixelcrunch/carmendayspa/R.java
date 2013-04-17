eb587ca6bd3050595a4fb43c001bff4ea8feb4c1
ianschmitz <ianschmitz@gmail.com>
2013-04-17 14:27:17 -0700

    added content to about page

--- Copy of CarmenDaySpa/gen/com/pixelcrunch/carmendayspa/R.java
+++ Copy of CarmenDaySpa/gen/com/pixelcrunch/carmendayspa/R.java
@@ -54,14 +54,15 @@ public final class R {
         public static final int places_selected=0x7f02001e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int btn_about=0x7f080006;
-        public static final int btn_booking=0x7f080003;
-        public static final int btn_contact=0x7f080005;
-        public static final int btn_directions=0x7f080004;
-        public static final int btn_products=0x7f080002;
-        public static final int btn_services=0x7f080001;
-        public static final int home_root=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int btn_about=0x7f080007;
+        public static final int btn_booking=0x7f080004;
+        public static final int btn_contact=0x7f080006;
+        public static final int btn_directions=0x7f080005;
+        public static final int btn_products=0x7f080003;
+        public static final int btn_services=0x7f080002;
+        public static final int home_root=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int about_layout=0x7f030000;
@@ -80,6 +81,7 @@ public final class R {
     }
     public static final class string {
         public static final int about=0x7f050008;
+        public static final int aboutCarmen=0x7f05000a;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int booking=0x7f050005;
