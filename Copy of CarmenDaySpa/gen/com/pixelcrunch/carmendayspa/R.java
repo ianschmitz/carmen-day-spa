00e9240fc38ce4bd6f216819727b2c3bb1c85a31
SeansAir <sean1991stewart@gmail.com>
2013-04-24 12:40:20 -0700

    Added back.png

--- Copy of CarmenDaySpa/gen/com/pixelcrunch/carmendayspa/R.java
+++ Copy of CarmenDaySpa/gen/com/pixelcrunch/carmendayspa/R.java
@@ -57,20 +57,35 @@ public final class R {
         public static final int stub=0x7f020021;
     }
     public static final class id {
+        public static final int ScrollView01=0x7f080002;
+        public static final int ScrollView02=0x7f08000b;
         public static final int aboutParagraph=0x7f080000;
-        public static final int action_settings=0x7f08000d;
+        public static final int action_settings=0x7f08001c;
         public static final int btnActionBarBack=0x7f080001;
-        public static final int btn_about=0x7f080008;
-        public static final int btn_booking=0x7f080005;
-        public static final int btn_contact=0x7f080007;
-        public static final int btn_directions=0x7f080006;
-        public static final int btn_products=0x7f080004;
-        public static final int btn_services=0x7f080003;
-        public static final int home_root=0x7f080002;
-        public static final int image=0x7f080009;
-        public static final int list=0x7f08000c;
-        public static final int text=0x7f08000a;
-        public static final int textPrice=0x7f08000b;
+        public static final int btnSendFeedback=0x7f08000a;
+        public static final int btn_about=0x7f080017;
+        public static final int btn_booking=0x7f080014;
+        public static final int btn_contact=0x7f080016;
+        public static final int btn_directions=0x7f080015;
+        public static final int btn_products=0x7f080013;
+        public static final int btn_services=0x7f080012;
+        public static final int etAddCommentsBooking=0x7f080009;
+        public static final int etBookingFirst=0x7f080004;
+        public static final int etBookingLast=0x7f080005;
+        public static final int etContactFirst=0x7f08000d;
+        public static final int etContactLast=0x7f08000e;
+        public static final int etEmailBooking=0x7f080007;
+        public static final int etFeedback=0x7f080010;
+        public static final int etPhoneBooking=0x7f080008;
+        public static final int etService=0x7f080006;
+        public static final int home_root=0x7f080011;
+        public static final int image=0x7f080018;
+        public static final int list=0x7f08001b;
+        public static final int ratingBar=0x7f08000f;
+        public static final int text=0x7f080019;
+        public static final int textPrice=0x7f08001a;
+        public static final int tvBookingTitle=0x7f080003;
+        public static final int tvContactTitle=0x7f08000c;
     }
     public static final class layout {
         public static final int about_layout=0x7f030000;
@@ -92,14 +107,26 @@ public final class R {
         public static final int about=0x7f050008;
         public static final int aboutCarmen=0x7f05000a;
         public static final int action_settings=0x7f050001;
+        public static final int additionalComments=0x7f050016;
         public static final int app_name=0x7f050000;
+        public static final int bookButton=0x7f050017;
         public static final int booking=0x7f050005;
+        public static final int bookingFirstName=0x7f050011;
+        public static final int bookingLastName=0x7f050012;
+        public static final int bookingTitle=0x7f050010;
         public static final int btnMore=0x7f05000b;
         public static final int contact=0x7f050004;
+        public static final int contactFirstName=0x7f05000d;
+        public static final int contactLastName=0x7f05000e;
+        public static final int contactTitle=0x7f05000c;
+        public static final int customerEmail=0x7f050014;
+        public static final int customerPhone=0x7f050015;
         public static final int directions=0x7f050006;
+        public static final int feedback=0x7f05000f;
         public static final int footerText=0x7f050009;
         public static final int hello_world=0x7f050002;
         public static final int products=0x7f050007;
+        public static final int requestedService=0x7f050013;
         public static final int services=0x7f050003;
     }
     public static final class style {
