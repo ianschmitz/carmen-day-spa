f3d0e575384d35d4748d9040c7c9fb336b54e13a
SeansAir <sean1991stewart@gmail.com>
2013-04-24 14:00:46 -0700

    Changes to Contact and Booking Forms, Fixed Rating bar

--- Copy of CarmenDaySpa/src/com/pixelcrunch/carmendayspa/ContactActivity.java
+++ Copy of CarmenDaySpa/src/com/pixelcrunch/carmendayspa/ContactActivity.java
@@ -2,7 +2,10 @@ package com.pixelcrunch.carmendayspa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContactActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -10,8 +13,30 @@ public class ContactActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contact_layout);
-		
+
 		TextView actionBarTitle = (TextView) findViewById(R.id.tvActionBarTitle);
 		actionBarTitle.setText(R.string.contact);
+
+		/**
+		 * Display Rating Update in a Toast
+		 */
+
+		final RatingBar ratingbar = (RatingBar) findViewById(R.id.ratingbar);
+		ratingbar.setStepSize((float) 1); //
+		ratingbar.setRating((float) 5); // set rating which u want to display
+
+		ratingbar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+			public void onRatingChanged(RatingBar ratingBar, float rating,
+					boolean fromUser) {
+				Toast.makeText(ContactActivity.this, "New Rating: " + rating,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		/**
+		 * 
+		 */
+
 	}
+
 }
--- Copy of CarmenDaySpa/src/com/pixelcrunch/carmendayspa/DirectionsActivity.java
+++ Copy of CarmenDaySpa/src/com/pixelcrunch/carmendayspa/DirectionsActivity.java
@@ -2,6 +2,7 @@ package com.pixelcrunch.carmendayspa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DirectionsActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +10,8 @@ public class DirectionsActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.directions_layout);
+		
+		TextView actionBarTitle = (TextView) findViewById(R.id.tvActionBarTitle);
+		actionBarTitle.setText(R.string.directions);
 	}
 }
